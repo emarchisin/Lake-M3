f802685666369ee28a37f46808abafb3fe4f9611
Sam Blackburn <srblackburn@wisc.edu>
2026-03-31 13:38:55 -0500

    slightly lower dystrophic f_sod

--- Project/gradients/prep_config.py
+++ Project/gradients/prep_config.py
@@ -78,7 +78,7 @@ tp_oc_pairs = [
     ('tp/low_tp.csv', 1, 2e-6),
     ('tp/med_tp.csv', 1, 7e-6),
     ('tp/high_tp.csv', 15, 2e-5),
-    ('tp/med_tp.csv', 30, 2e-5)
+    ('tp/med_tp.csv', 30, 1e-5)
 ]
 
 # Generate the 864 combinations
